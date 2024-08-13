7884a15bfd60fb136b7606154e53c52d3f2dd2d7
Amber Nguyen <amber.quynh.nguyen@gmail.com>
2024-08-13 16:50:10 +0800

    first change

--- weather.py
+++ weather.py
@@ -35,7 +35,9 @@ def convert_f_to_c(temp_in_fahrenheit):
     Returns:
         A float representing a temperature in degrees Celcius, rounded to 1 decimal place.
     """
-    pass
+
+    temp_in_celcius = (float(temp_in_fahrenheit)-32)*(5/9) 
+    return round(temp_in_celcius,1)
 
 
 def calculate_mean(weather_data):
